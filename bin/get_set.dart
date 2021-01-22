999634a8bf1df8f215de7d424a994f3ee91ec991
Hector Saldaña <hectorsalbe@hotmail.com>
2021-01-22 14:10:42 -0600

    Commit inicial

---
+++ bin/get_set.dart
@@ -0,0 +1,18 @@
+import 'triangulo.dart';
+import 'persona.dart';
+
+void main(List<String> arguments) {
+  /*
+  Triangulo triangulo = new Triangulo();
+
+  triangulo.base = 15.8;
+  triangulo.altura = 23.5;
+
+  print('El área del triángulo es: ${triangulo.area}');
+  */
+  Persona p = new Persona();
+
+  p.nombre = 'Juan';
+
+  print('El nombre es: ${p.nombrePersona}');
+}
